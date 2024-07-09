29c1679f747b6313e11b63d41a44359a1352d8a5
Jorge Matos <jisapjisap@gmail.com>
2024-07-09 17:34:41 +0200

    treatments

--- app/treatments/page.jsx
+++ app/treatments/page.jsx
@@ -20,13 +20,23 @@ const Treatments = () => {
       <div className='container mx-auto flex items-center pt-48 pb-12 xl:pt-32 xl:pb-0'>
         <div className='w-full h-full flex items-center justify-between'>
           {/* text */}
-          <div className='flex flex-col items-start xl:max-w-[650px] text-center xl:text-left mx-auto'>
+          <motion.div 
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaveHandler} 
+            initial={{opacity: 0, x: -60}}
+            animate={{
+              opacity:1,
+              x: 0,
+              transition: {delay: 2, duration: 0.8, ease: 'easeInOut'},
+            }}
+            className='flex flex-col items-start xl:max-w-[650px] text-center xl:text-left mx-auto'
+          >
             <h2 className='h2 mb-6 mx-auto ,max-w-[540px] xl:mas-w-none'>Indulge in out Luxurious Treatments</h2>
             <p className='lead max-w-[600px] mx-auto xl:mx-0'>
               Tailored skincare solutions for a healty complexion, offering customized care for radiant skin.
             </p>
             {/* items */}
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-[20px] mb-14 mx-auto xl:mx-0'>
               {/* item */}
               <div className='flex-1 flex flex-col justify-center items-center xl:items-start'>
                 <div className='flex items-center gap-[12px] mb-2'>
@@ -35,11 +45,54 @@ const Treatments = () => {
                     Classic Facial
                   </h3>
                 </div>
+                <p className='pl-6 tex-[15px]'>
+                  Deep cleansing exfoliation and hydration for a refreshed complexion
+                </p>
               </div>
+              {/* end item */}
+              {/* item */}
+              <div className='flex-1 flex flex-col justify-center items-center xl:items-start'>
+                <div className='flex items-center gap-[12px] mb-2'>
+                  <div className='w-[14px] h-[14px] bg-accent rounded-tl-[28px] rounded-bl-[28px] rounded-br-[22px] rounded-tr-[4px]'></div>
+                  <h3 className='text-2xl'>
+                    Chemical Peel
+                  </h3>
+                </div>
+                <p className='pl-6 tex-[15px]'>
+                  Exfoliation treatment to improve skin texture and tone.
+                </p>
+              </div>
+              {/* end item */}
+              {/* item */}
+              <div className='flex-1 flex flex-col justify-center items-center xl:items-start'>
+                <div className='flex items-center gap-[12px] mb-2'>
+                  <div className='w-[14px] h-[14px] bg-accent rounded-tl-[28px] rounded-bl-[28px] rounded-br-[22px] rounded-tr-[4px]'></div>
+                  <h3 className='text-2xl'>
+                    Eyebrow Waxing/shaping
+                  </h3>
+                </div>
+                <p className='pl-6 tex-[15px]'>
+                  Sculpts the eyebrows and removes unwanted hair using wax for a defined, pleasing contour.
+                </p>
+              </div>
+              {/* end item */}
+              {/* item */}
+              <div className='flex-1 flex flex-col justify-center items-center xl:items-start'>
+                <div className='flex items-center gap-[12px] mb-2'>
+                  <div className='w-[14px] h-[14px] bg-accent rounded-tl-[28px] rounded-bl-[28px] rounded-br-[22px] rounded-tr-[4px]'></div>
+                  <h3 className='text-2xl'>
+                    HydraFacial
+                  </h3>
+                </div>
+                <p className='pl-6 tex-[15px]'>
+                  Multi-step treatment for instant hydration grow
+                </p>
+              </div>
+              {/* end item */}
             </div>
             {/* btn */}
             <button className='btn mx-auto xl:mx-0'>Discover more</button>
-          </div>
+          </motion.div>
           {/* image */}
           <div className='hidden relative xl:flex w-[384px] h-[534px]'>
             <Image 
