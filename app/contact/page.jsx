33536c184146bd0041366720090e09e3f9406af2
Jorge Matos <jisapjisap@gmail.com>
2024-07-10 18:46:25 +0200

    Contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,17 +1,33 @@
 "use client"
 
+import React, { useContext } from 'react'
+import { CursorContext } from '@/components/CursorContext'
 import { motion } from 'framer-motion'
-import React from 'react'
+import Image from 'next/image'
+import Form from '@/components/Form'
+
 
 
 const Treatments = () => {
+
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 2 } }}
       className='min-h-screen flex items-center overflow-x-hidden'
     >
-      Treatments
+      <div className='container mx-auto pt-48 pb-12 xl:pt-32 xl:pb-0'>
+        <div className='flex flex-col gap-12 xl:flex-row h-full'>
+          {/* text */}
+          <div>text</div>
+          {/* form */}
+          <div>
+            <Form />
+          </div>
+        </div>
+      </div>
     </motion.section>
   )
 }
