14a7e3c43df06b5e5835a19c39eca94801071f11
Jorge Matos <jisapjisap@gmail.com>
2024-07-10 19:03:31 +0200

    Contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -28,14 +28,53 @@ const Treatments = () => {
             {/* items */}
             <div className='flex flex-col items-center xl:items-start gap-12'>
               {/* item */}
-              <div>
+              <div className='flex items-start gap-4'>
                 {/* icon */}
                 <div className='relative w-[36px] h-[36px]'>
                   <Image src="/assets/contact/pin.svg" fill alt="" />
                 </div>
+                <div className='pt-1 flex-1'>
+                  <h4 className='h4 mb-2'>Address</h4>
+                  <p className='leading-relaxed'>
+                    123/45 Elm St, Suite 800
+                    <br />
+                    Los Angeles, CA 90012
+                  </p> 
+                </div>
               </div>
-              <div>item2</div>
-              <div>item3</div>
+              {/* end item */}
+              {/* item */}
+              <div className='flex items-start gap-4'>
+                {/* icon */}
+                <div className='relative w-[36px] h-[36px]'>
+                  <Image src="/assets/contact/pin.svg" fill alt="" />
+                </div>
+                <div className='pt-1 flex-1'>
+                  <h4 className='h4 mb-2'>Address</h4>
+                  <p className='leading-relaxed'>
+                    123/45 Elm St, Suite 800
+                    <br />
+                    Los Angeles, CA 90012
+                  </p>
+                </div>
+              </div>
+              {/* end item */}
+              {/* item */}
+              <div className='flex items-start gap-4'>
+                {/* icon */}
+                <div className='relative w-[36px] h-[36px]'>
+                  <Image src="/assets/contact/pin.svg" fill alt="" />
+                </div>
+                <div className='pt-1 flex-1'>
+                  <h4 className='h4 mb-2'>Address</h4>
+                  <p className='leading-relaxed'>
+                    123/45 Elm St, Suite 800
+                    <br />
+                    Los Angeles, CA 90012
+                  </p>
+                </div>
+              </div>
+              {/* end item */}
             </div>
           </div>
           {/* form */}
